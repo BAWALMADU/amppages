5a657235e80531cbf42cf34ed38714342d67dc7e
BAWALMADU <ikanbawalmadu@gmail.com>
2025-08-20 19:04:19 +0700

    Update madu.js

--- pages/madu.js
+++ pages/madu.js
@@ -1,10 +1,1 @@
-export const config = { amp: true }
 
-export default function AmpPage() {
-  return (
-    <div>
-      <h1>Halo dari AMP ⚡</h1>
-      <p>Ini halaman AMP pakai Next.js</p>
-    </div>
-  )
-}
